8eb4669ba69dd31308a117072338dd6aa0427318
VincentWLi1997 <vincentwl1997@gmail.com>
2024-04-16 19:21:48 -0700

    Removed phantom submit button, hid API key in environment, deleted python cache from project file

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import story_image_generation as story_image_generation
 import story_voice_generation as story_voice_generation
 
 #API Key goes here
-client = OpenAI(api_key="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")
+client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
 # create our streamlit app
 with st.form(key = "chat"):
@@ -23,7 +23,7 @@ with st.form(key = "chat"):
         #Generate story, title, and prompt for image
         story=story_text_generation.get_completion(prompt)
         title_prompt = "The following is a bedtime story for a young child. Generate a title for the story that is 3 words or less. Write only the three word title and nothing else:\n" + story
-        image_prompt="The following is a bedtime story for a young child. Generate a picture without any text inside that goes with the story:\n" + story
+        image_prompt= "The following is a bedtime story for a young child. Generate a picture without any text inside that goes with the story:\n" + story
         title = story_text_generation.get_completion(title_prompt)
         
         #Write out title and story into app
--- story_voice_generation.py
+++ story_voice_generation.py
@@ -6,9 +6,6 @@ import requests
 from openai import OpenAI
 from pathlib import Path
 
-st.markdown("# Page 5 Text to Speech ❄️")
-st.sidebar.markdown("# Page 5 Text to Speech ❄️")
-
 speech_file_path = Path(__file__).parent / "newfile.mp3"
 
 client = OpenAI(api_key="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")
@@ -22,11 +19,3 @@ def text_to_speech(text,path):
     response.stream_to_file(speech_file_path)
 
 text = ''
-
-# Create a button
-if st.button('Submit'):
-    text_to_speech(text, speech_file_path)
-
-    audio_file = open(speech_file_path, 'rb')
-    audio_bytes = audio_file.read()
-    st.audio(audio_bytes, format='audio/mp3')
